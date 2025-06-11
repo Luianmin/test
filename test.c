0fde8e65c7bc1ea577ad2154c66f7a0e5138fe62
Lumin <Luyanming307@gmail.com>
2025-06-11 17:10:27 +0800

    1

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main() {
+    int a = 5;
+    for(int i=a;a<0;a++){
+        printf("%d\n", a);
+    }
     printf("Hello, World!\n");
-    printf("This project uses the C17 language standard version.\n");
     return 0;
 }
